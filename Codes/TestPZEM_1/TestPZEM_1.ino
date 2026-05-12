e8733e4cd79ee51b671d104b5895171d10efba0c
gdysidik <gundik178@gmail.com>
2026-05-12 22:30:44 +0700

    menambahkan folder Codes ke repo

---
+++ Codes/TestPZEM_1/TestPZEM_1.ino
@@ -0,0 +1,57 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+If only RX and TX pins are passed to the constructor, software 
+serial interface will be used for communication with the module.
+
+*/
+
+#include <PZEM004Tv30.h>
+
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+
+PZEM004Tv30 pzem(&Serial2, PZEM_RX_PIN, PZEM_TX_PIN, 0x01);
+
+void setup() {
+    /* Debugging serial */
+    Serial.begin(115200);
+}
+
+void loop() {
+
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
+        Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+    }
+
+    Serial.println();
+    delay(2000);
+}
